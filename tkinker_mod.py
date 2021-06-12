8b58c138ace848d2dc0afc7e7e710e21617eba08
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-12 14:38:13 +0200

    right/left click on button

--- tkinker_mod.py
+++ tkinker_mod.py
@@ -11,10 +11,22 @@ root.geometry('800x700')
 root.configure(background='black')
 
 # events
+def but_left(event):
+    print("left click")
+
+def but_right(event):
+    print("right click")
 
 # images 
 
 # elements
+b1 = tkinter.Button(root,text = "BUTTON1")
+b1.bind('<Button-3>',but_right)
+b1.pack()
+
+b2 = tkinter.Button(root,text = "BUTTON2")
+b2.bind('<Button-1>',but_left)
+b2.pack()
 
 # exit from loop
 root.mainloop()
