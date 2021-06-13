5210ce08b396d9143366eb278e720f48c4663e17
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-13 14:36:53 +0200

    basic forms of statment

--- first_gui.py
+++ first_gui.py
@@ -1,1 +1,13 @@
 import tkinter
+from PIL import Image, ImageTk
+
+# settings of main root PANEL
+root = tkinter.Tk()
+root.title("ANIMAL IMAGES ON THE PANEL")
+root.geometry('800x700')
+root.configure(background='black')
+
+
+
+# exit from loop of the window PANEL
+root.mainloop()
