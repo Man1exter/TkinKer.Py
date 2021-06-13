b027e22d4b2d6fd435bf2d434c18b7eb49c204c8
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-13 14:41:44 +0200

    duplicate from before file the code of file

--- first_gui.py
+++ first_gui.py
@@ -7,6 +7,17 @@ root.title("ANIMAL IMAGES ON THE PANEL")
 root.geometry('800x700')
 root.configure(background='black')
 
+# menu with click-list 
+main_menu = tkinter.Menu()
+root.config(menu = main_menu)
+
+# section on the menu
+file_menu = tkinter.Menu(main_menu)
+main_menu.add_cascade(label='ROLL', menu = file_menu)
+file_menu.add_command(label='New Animal')
+file_menu.add_command(label='New Car')
+file_menu.add_command(label='New House')
+file_menu.add_command(label='New Game')
 
 
 # exit from loop of the window PANEL
