b318444b23618adcefddf567f3ff685cb1ba75f3
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-13 11:10:10 +0200

    hide buttons and resize image

--- tkinker_mod.py
+++ tkinker_mod.py
@@ -13,27 +13,27 @@ root.configure(background='black')
 # background image / graphic
 # ----- size of image ----- #
 bg_image = Image.open('elephant.jpg')
-bg_image = bg_image.resize((500,500),Image.ANTIALIAS)
+bg_image = bg_image.resize((797,697),Image.ANTIALIAS)
 access_image = ImageTk.PhotoImage(bg_image)
 tkinter.Label(root,image = access_image).pack()
 
 # events
-def but_left(event):
-    print("left click")
+#def but_left(event):
+    #print("left click")
 
-def but_right(event):
-    print("right click")
+#def but_right(event):
+    #print("right click")
 
 # download images / graphics
 
 # elements
-b1 = tkinter.Button(root,text = "BUTTON1")
-b1.bind('<Button-3>',but_right)
-b1.pack()
+#b1 = tkinter.Button(root,text = "BUTTON1")
+#b1.bind('<Button-3>',but_right)
+#b1.pack()
 
-b2 = tkinter.Button(root,text = "BUTTON2")
-b2.bind('<Button-1>',but_left)
-b2.pack()
+#b2 = tkinter.Button(root,text = "BUTTON2")
+#b2.bind('<Button-1>',but_left)
+#b2.pack()
 
 # exit from loop
 root.mainloop()
