b3832fe60bf21dc0a57d0e53de6a0c41890dc127
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-17 11:53:55 +0200

    basic duplicate of stst

--- Butt_gui.py
+++ Butt_gui.py
@@ -1,3 +1,13 @@
 # by Man1exter ==> https://github.com/Man1exter
 import tkinter
 from PIL import Image, ImageTk
+
+# settings of main root
+root = tkinter.Tk()
+root.title("START TKINTER PROJECT")
+root.geometry('800x700')
+root.configure(background='black')
+
+
+# exit with [X]
+root.mainloop()
