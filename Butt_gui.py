ab980eeb6c315347aeac2bec7f939c87d893b2a9
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-17 12:49:53 +0200

    use grid delete pack

--- Butt_gui.py
+++ Butt_gui.py
@@ -1,4 +1,5 @@
 # by Man1exter ==> https://github.com/Man1exter
+
 import tkinter
 from PIL import Image, ImageTk
 
@@ -10,11 +11,13 @@ root.configure(background='black')
 
 # buttons with pack
 b1 = tkinter.Button(root,text="ANIMAL",bg="lightblue",width=10)
-b1.pack()
 b2 = tkinter.Button(root,text="CAR",bg="red",width=10)
-b2.pack()
 b3 = tkinter.Button(root,text="HOUSE",bg="yellow",width=10)
-b3.pack()
+
+# grid mesh cache
+b1.grid(row = 0,  column = 0)
+b2.grid(row = 0,  column = 1)
+b3.grid(row = 0,  column = 2)
 
 # functions with event
 
