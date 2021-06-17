f43cdbfa236850da6e6b67fc3eb7c5879f85f05d
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-17 12:24:17 +0200

    3 buttons with text

--- Butt_gui.py
+++ Butt_gui.py
@@ -9,6 +9,12 @@ root.geometry('800x700')
 root.configure(background='black')
 
 # buttons with pack
+b1 = tkinter.Button(root,text="ANIMAL",bg="lightblue",width=10)
+b1.pack()
+b2 = tkinter.Button(root,text="CAR",bg="red",width=10)
+b2.pack()
+b3 = tkinter.Button(root,text="HOUSE",bg="yellow",width=10)
+b3.pack()
 
 # functions with event
 
