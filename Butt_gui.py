a702ffe16b14fdbe34b06169859b9e27c5704a17
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-20 12:54:19 +0200

    last commit with one function to animals

--- Butt_gui.py
+++ Butt_gui.py
@@ -11,21 +11,27 @@ root.title("START TKINTER PROJECT")
 root.geometry('800x700')
 root.configure(background='black')
 
-# settings of image after click on button
-
 # random color
-color = ["blue","yellow","green","orange","purple","lightblue","olive","pink"]
+color = ["red","yellow","green","orange","purple","lightblue","olive","pink"]
 colors = random.choice(color)
 
+# functions with event to add images after click
+def change_image_animal():
+   pack_animal = ['./animals/elephant.jpg','./animals/fhdf.jpg','./animals/hgfhgfhggf.jpg','./animals/jghjhjhgj.jpg','./animals/jhggjhj.jpg']
+   bg_image = Image.open(random.choice(pack_animal))
+   bg_image = bg_image.resize((800,700),Image.ANTIALIAS)
+   access_image = ImageTk.PhotoImage(bg_image)
+   tkinter.Label(root,image = access_image).pack()
+
 # buttons with pack
-b1 = tkinter.Button(root,text="ANIMAL",bg=colors,width=10,font=('Arial', 12))
-b2 = tkinter.Button(root,text="CAR",bg=colors,width=10,font=('Arial', 12))
-b3 = tkinter.Button(root,text="HOUSE",bg=colors,width=10,font=('Arial', 12))
-b4 = tkinter.Button(root,text="JOB",bg=colors,width=10,font=('Arial', 12))
-b5 = tkinter.Button(root,text="COUNTRY",bg=colors,width=10,font=('Arial', 12))
-b6 = tkinter.Button(root,text="CLUB",bg=colors,width=10,font=('Arial', 12))
-b7 = tkinter.Button(root,text="NAME",bg=colors,width=10,font=('Arial', 12))
-b8 = tkinter.Button(root,text="WEATHER",bg=colors,width=10,font=('Arial', 12))
+b1 = tkinter.Button(root,text="ANIMAL",bg = colors,width = 10,font = ('Arial', 12),command = change_image_animal)
+b2 = tkinter.Button(root,text="CAR",bg = colors,width = 10,font = ('Arial', 12))
+b3 = tkinter.Button(root,text="HOUSE",bg = colors,width = 10,font = ('Arial', 12))
+b4 = tkinter.Button(root,text="JOB",bg = colors,width = 10,font = ('Arial', 12))
+b5 = tkinter.Button(root,text="COUNTRY",bg = colors,width = 10,font = ('Arial', 12))
+b6 = tkinter.Button(root,text="CLUB",bg = colors,width = 10,font = ('Arial', 12))
+b7 = tkinter.Button(root,text="NAME",bg = colors,width = 10,font = ('Arial', 12))
+b8 = tkinter.Button(root,text="WEATHER",bg = colors,width = 10,font = ('Arial', 12))
 
 # grid mesh cache
 b1.grid(row = 1,  column = 0)
@@ -36,8 +42,6 @@ b5.grid(row = 1,  column = 4)
 b6.grid(row = 0,  column = 5)
 b7.grid(row = 1,  column = 6)
 b8.grid(row = 0,  column = 7)
-
-# functions with event to add images after click
 
 # images (download)
 def fun_ani():
