b8a9ccd07ee403e33c5c574e9901936d0881c79e
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-21 14:09:58 +0200

    second function with car images

--- Butt_gui.py
+++ Butt_gui.py
@@ -19,14 +19,21 @@ colors = random.choice(color)
 def change_image_animal():
    pack_animal = ['./animals/elephant.jpg','./animals/fhdf.jpg','./animals/hgfhgfhggf.jpg','./animals/jghjhjhgj.jpg','./animals/jhggjhj.jpg']
    bg_image = Image.open(random.choice(pack_animal))
-   bg_image = bg_image.resize((800,700),Image.ANTIALIAS)
+   # bg_image = bg_image.resize((800,700),Image.ANTIALIAS)
    access_image = ImageTk.PhotoImage(bg_image)
    tkinter.Label(root,image = access_image).pack()
 # bug / fix / error ===> feature :D
 
+def car_change():
+   pack_car = ['/cars/2020-Toyota-Supra-13.jpg','/cars/27142_social-supercar-le-auto-dei-sogni.jpg','/cars/9370122a.jpg','/cars/mclaren-720s_0.jpg','/cars/SharpJan20_Supercars_feat.jpg','/cars/MW-AQ797_bugatt_20120410172421_MG.jpg']
+   bg_image = Image.open(random.choice(pack_car))
+   bg_image = bg_image.resize((800,700),Image.ANTIALIAS)
+   access_image = ImageTk.PhotoImage(bg_image)
+   tkinter.Label(root,image = access_image).pack()
+
 # buttons with pack
 b1 = tkinter.Button(root,text="ANIMAL",bg = colors,width = 10,font = ('Arial', 12),command = change_image_animal)
-b2 = tkinter.Button(root,text="CAR",bg = colors,width = 10,font = ('Arial', 12))
+b2 = tkinter.Button(root,text="CAR",bg = colors,width = 10,font = ('Arial', 12),command = car_change)
 b3 = tkinter.Button(root,text="HOUSE",bg = colors,width = 10,font = ('Arial', 12))
 b4 = tkinter.Button(root,text="JOB",bg = colors,width = 10,font = ('Arial', 12))
 b5 = tkinter.Button(root,text="COUNTRY",bg = colors,width = 10,font = ('Arial', 12))
@@ -43,14 +50,6 @@ b5.grid(row = 1,  column = 4)
 b6.grid(row = 0,  column = 5)
 b7.grid(row = 1,  column = 6)
 b8.grid(row = 0,  column = 7)
-
-# images (download)
-def fun_ani():
-   pack_animal = ['/ima/elephant.jpg','/ima/fhdf.jpg','/ima/jhggjhj.jpg','/ima/jghjhjhgh.jpg','/ima/hgfhgfhggf.jpg']
-   bg_image = Image.open(random.choice(pack_animal))
-   bg_image = bg_image.resize((800,700),Image.ANTIALIAS)
-   access_image = ImageTk.PhotoImage(bg_image)
-   tkinter.Label(root,image = access_image).pack()
 
 # exit with [X]
 root.mainloop()
