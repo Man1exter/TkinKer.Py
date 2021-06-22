2ed2381421191be15e39df7e6b464a80354ed7dc
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-22 10:57:50 +0200

    basic root

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 import tkinter
 
-root = tkinter.Tkinter()
+root = tkinter.Tk()
+root.title("GRAPHICS AND IMAGES ON THE PANEL / BACKGROUND")
+root.geometry('800x700')
+root.configure(background='black')
