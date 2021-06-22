fd26dc98dbda13942d2019e2cc8d594e50c72e81
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-22 11:56:06 +0200

    rename panel and add mainloop

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import tkinter
 
 root = tkinter.Tk()
-root.title("GRAPHICS AND IMAGES ON THE PANEL / BACKGROUND")
+root.title("TESTING BUTTON CHANGE SOMETHING")
 root.geometry('800x700')
 root.configure(background='black')
+
+
+root.mainloop()
