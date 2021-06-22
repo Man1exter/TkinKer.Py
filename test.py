05abd8fbdb53e457e651a89a901afc5e62e16fa2
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-22 11:59:49 +0200

    add arguments to button(tkinter)

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import tkinter
 root = tkinter.Tk()
 root.title("TESTING BUTTON CHANGE SOMETHING")
 root.geometry('800x700')
-root.configure(background='black')
+root.configure(background = 'green')
 
+button_test = tkinter.Button(root, text = 'CLICK',bg = 'yellow', width = 10, font = ('ARIAL',15))
+button_test.pack()
 
 root.mainloop()
