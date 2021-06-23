32b8c37a749ad30931226ddcb275d180f220c1e0
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-23 17:58:02 +0200

    commit

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ root.title("TESTING BUTTON CHANGE SOMETHING")
 root.geometry('800x700')
 root.configure(background = 'green')
 
-button_test = tkinter.Button(root, text = 'CLICK',bg = 'yellow', width = 10, font = ('ARIAL',15))
+button_test = tkinter.Button(root, text = 'CLICK',bg = 'green', width = 11, font = ('ARIAL',15))
 button_test.pack()
 
 root.mainloop()
