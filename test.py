2e505e0b3534546df1de73e0b462f97a29cc2225
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-27 16:42:29 +0200

    change background with click on the button

--- test.py
+++ test.py
@@ -5,7 +5,9 @@ root.title("TESTING BUTTON CHANGE SOMETHING")
 root.geometry('800x700')
 root.configure(background = 'green')
 
-button_test = tkinter.Button(root, text = 'CLICK',bg = 'green', width = 11, font = ('ARIAL',15))
-button_test.pack()
+def background_change():
+    root.configure(background = 'yellow')
+
+button_test = tkinter.Button(root, text = 'CLICK',bg = 'green', width = 11, font = ('ARIAL',15), command=background_change).pack()
 
 root.mainloop()
