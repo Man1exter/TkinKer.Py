532e2c95766e5c4b185fe4df865255bf9447bcc8
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-27 17:15:13 +0200

    add random color with if or while

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
-from tkinter import *
+from tkinter import * 
 import random
 
 root = Tk()
 root.title("TESTING BUTTON CHANGE SOMETHING")
 root.geometry('800x700')
 root.configure(background = 'green')
-img = PhotoImage(file="bll.jpg")
+
+def change_color(i = 0):
+    if i < 100:
+        colors = ('red', 'blue', 'green', 'black', "pink", "blue")
+        root.config(bg=colors[i])
+        root.after(500, change_color, i+1)
 
 def background_change():
     color_list = ["yellow","black","orange","brown","blue","purple"]
     random_color = random.choice(color_list)
     root.configure(background = random_color)
 
-def background_change_image():
-    image_list = ["/animals/elephant.jpg","/animals/aninimal.jpg","/animals/hgfhgfhggf.jpg","/animals/jghjhjhgj.jpg","/animals/jhggjhj.jpg"]
-    filename = root(file = random.choice(image_list)) 
-    root.configure(background = filename)
-
 button_test = Button(root, text = 'COLOR',bg = 'green', width = 11, font = ('ARIAL',15), command=background_change).pack()
-button_test_image = Button(root, text = 'ANIMAL',bg = 'lightgreen', width = 11, font = ('ARIAL',15), command=background_change_image).pack()
 
+change_color()
 root.mainloop()
