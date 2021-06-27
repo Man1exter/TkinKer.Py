ba473f1d0f1654d18a50c100e29963c4c85ed7db
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-27 16:47:36 +0200

    all button-click is done

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import tkinter
+import random
 
 root = tkinter.Tk()
 root.title("TESTING BUTTON CHANGE SOMETHING")
@@ -6,8 +7,10 @@ root.geometry('800x700')
 root.configure(background = 'green')
 
 def background_change():
-    root.configure(background = 'yellow')
+    color_list = ["yellow","black","orange","brown","blue","purple"]
+    random_color = random.choice(color_list)
+    root.configure(background = random_color)
 
-button_test = tkinter.Button(root, text = 'CLICK',bg = 'green', width = 11, font = ('ARIAL',15), command=background_change).pack()
+button_test = tkinter.Button(root, text = 'CHANGE',bg = 'green', width = 11, font = ('ARIAL',15), command=background_change).pack()
 
 root.mainloop()
