2ddc87dd7c2cca1d515bf6dbe46f292434cea31f
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-28 20:37:38 +0200

    next button the same function

--- test.py
+++ test.py
@@ -22,6 +22,7 @@ def background_change():
     root.configure(background = random_color)
 
 button_test = Button(root, text = 'COLOR',bg = 'green', width = 11, font = ('ARIAL',15), command=background_change).pack()
+button_test = Button(root, text = 'COLOR',bg = 'green', width = 11, font = ('ARIAL',15), command=background_change).pack()
 
 change_color()
 root.mainloop()
